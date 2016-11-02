a28b0a265c3834d497394b7c942c1fdcb244f8d2
jesusgabriel <amelo@fvi.edu>
2016-11-01 20:43:53 -0400

    new

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+$(".button").click(function(){
+var x = $("input[name='name']:checked").val();
+if (x == 0) {
+  window.location = "file:///C:/Users/jesus/Documents/Projects/test-based-adventure/index2.html";
+}
+if (x == 1) {
+  window.location = "file:///C:/Users/jesus/Documents/Projects/test-based-adventure/index3.html";
+}
+if (x == 2) {
+  window.location = "file:///C:/Users/jesus/Documents/Projects/test-based-adventure/index3.html";
+}
+if (x == 3) {
+  window.location = "file:///C:/Users/jesus/Documents/Projects/test-based-adventure/index2.html";
+}
+});
